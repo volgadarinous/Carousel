a6ceb4ea4eb34c31d42047c5f682d96f1b4dba65
volgadarinous <hasti_s_v@yahoo.com>
2018-06-05 13:45:57 +0200

    Update makecarousel.js

--- site/makecarousel.js
+++ site/makecarousel.js
@@ -1,13 +1,23 @@
 //var fs = require('fs');
 //var files = fs.readdirSync('/Pantoscope');
 var m = []
+const testFolder = './Pantoscope/';
+const fs = require('fs');
+
+
 
 $(document).ready(function(){  
- $.get("./Pantoscope/", function(data) {
-	 m.push(data)
- })
+//  $.get("./Pantoscope/", function(data) {
+// 	 m.push(data)
+//  })
 	
 //m = ['la.jpg','chicago.jpg']
+	
+  fs.readdirSync(testFolder).forEach(file => {
+     m.push(file)
+     console.log(file);
+   })
+
   for(var i=0 ; i<m.length ; i++) {
     $('<div class="item"><img src="/Pantoscope/'+m[i]+'"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
     $('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
