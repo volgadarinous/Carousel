ba59e5a4762fb8b140ca60158a8e654cbd78afc4
volgadarinous <hasti_s_v@yahoo.com>
2018-06-05 13:27:30 +0200

    Add files via upload

---
+++ site/makecarousel.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){  
+	 m = ['la.jpg','chicago.jpg','ny.jpg']
+  for(var i=0 ; i<m.length ; i++) {
+    $('<div class="item"><img src="'+m[i]+'"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
+    $('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
+
+  }
+  $('.item').first().addClass('active');
+  $('.carousel-indicators > li').first().addClass('active');
+  $('#myCarousel').carousel();
+});
