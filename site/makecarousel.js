153800369813da1220300651ddefbfa7f4632489
volgadarinous <hasti_s_v@yahoo.com>
2018-06-06 11:33:26 +0200

    Update makecarousel.js

--- site/makecarousel.js
+++ site/makecarousel.js
@@ -7,36 +7,41 @@ var testFolder = './Pantoscope/';
 
 
 $(document).ready(function(){  
-//  $.get("./Pantoscope/", function(data) {
-// 	 m.push(data)
-//  })
-var url = window.location.href;     // Returns full URL
-console.log(url)
-var params = url.split('?');
-var url = params[0]
-var folder = params[1]
-if(folder){
-	testFolder = '/'+folder+'/'
-	console.log('param is:'+testFolder)
-}
+	//  $.get("./Pantoscope/", function(data) {
+	// 	 m.push(data)
+	//  })
+	var url = window.location.href;     // Returns full URL
+	console.log(url)
+	var params = url.split('?');
+	var url = params[0]
+	var folder = params[1]
+	if(folder){
+		testFolder = '/'+folder+'/'
+		console.log('param is:'+testFolder)
+	}
 
 	
-//m = ['la.jpg','chicago.jpg']
-m= ['Figure1-1.png','Figure2-1.png','Figure3-1.png','Figure4-1.png','Figure5-1.png','Figure6-1.png','Figure7-1.png']
+	//Placing images
+	m= ['Figure1-1.png','Figure2-1.png','Figure3-1.png','Figure4-1.png','Figure5-1.png','Figure6-1.png','Figure7-1.png']
 	
-//   fs.readdir(testFolder).forEach(file => {
-//      m.push(file)
-//      console.log(file);
-//    })
+	//   fs.readdir(testFolder).forEach(file => {
+	//      m.push(file)
+	//      console.log(file);
+	//    })
 
-  for(var i=0 ; i<m.length ; i++) {
-  var filename = url.slice(0,-10)+testFolder+m[i]
-  console.log('filename is:'+filename)
-    $('<div class="item"><img src="'+filename+'" style="height: 100%; margin: auto; object-fit:contain;"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
-    $('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
+  	for(var i=0 ; i<m.length ; i++) {
+  	var filename = url.slice(0,-10)+testFolder+m[i]
+  	console.log('filename is:'+filename)
+    	$('<div class="item"><img src="'+filename+'" style="height: 100%; margin: auto; object-fit:contain;"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
+    	$('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
 
-  }
-  $('.item').first().addClass('active');
-  $('.carousel-indicators > li').first().addClass('active');
-  $('#myCarousel').carousel();
+  	}
+	//Placing the device model video
+	var videofilename = url.slice(0,-10)+testFolder+'video.mp4'
+	$('<div class="item"><video autoplay loop><source src="'+videofilename+'" style="height=100%; margin:auto; object-fit:contain;" type="video/mp4"></video><div class="carousel-caption"></div> </div>').appendTo('.carousel-inner');
+	$('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
+
+	$('.item').first().addClass('active');
+	$('.carousel-indicators > li').first().addClass('active');
+	$('#myCarousel').carousel();
 });
