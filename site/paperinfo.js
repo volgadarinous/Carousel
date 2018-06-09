15d722466f0558ccbcd3786e6b61234d8b97a753
volgadarinous <hasti_s_v@yahoo.com>
2018-06-09 14:19:52 +0200

    Add files via upload

---
+++ site/paperinfo.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){ 
+
+	var url = window.location.href;     // Returns full URL
+	console.log(url)
+	var params = url.split('?');
+	var url = params[0]
+	var folder = params[1]
+	if(folder){
+		testFolder = '/'+folder+'/'
+		console.log('param is:'+testFolder)
+	}
+
+	$('#abstract').innerHTML = "Helloooo"
+
+	// for(var i=0 ; i<m.length ; i++) {
+ //  		var filename = url.slice(0,-10)+testFolder+m[i]
+ //  		console.log('filename is:'+filename)
+	// 	$('<div class="item"><img src="'+filename+'"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
+	// 	$('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
+	
+// 	var img = new Image();
+// 	img.src = filename;
+
+// 	img.onerror = function(){ // Failed to load
+// 		console.log('could not loadd:'+filename)
+// 	};
+// 	img.onload = function(){ // Loaded successfully
+// 		console.log('displaying:'+filename)
+// 	  	$('<div class="item"><img src="'+filename+'"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
+//     		$('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
+
+// 	};
+    
+  	}
+});
