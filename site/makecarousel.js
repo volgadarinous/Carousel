c7bfebc0c1e2e17f3694f5427ba1824feeda8bdc
volgadarinous <hasti_s_v@yahoo.com>
2018-06-09 14:09:11 +0200

    Update makecarousel.js

--- site/makecarousel.js
+++ site/makecarousel.js
@@ -30,20 +30,23 @@ $(document).ready(function(){
 	//    })
 
   	for(var i=0 ; i<m.length ; i++) {
-  	var filename = url.slice(0,-10)+testFolder+m[i]
-  	console.log('filename is:'+filename)
-	var img = new Image();
-	img.src = filename;
+  		var filename = url.slice(0,-10)+testFolder+m[i]
+  		console.log('filename is:'+filename)
+		$('<div class="item"><img src="'+filename+'"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
+		$('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
+	
+// 	var img = new Image();
+// 	img.src = filename;
 
-	img.onerror = function(){ // Failed to load
-		console.log('could not loadd:'+filename)
-	};
-	img.onload = function(){ // Loaded successfully
-		console.log('displaying:'+filename)
-	  	$('<div class="item"><img src="'+filename+'"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
-    		$('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
+// 	img.onerror = function(){ // Failed to load
+// 		console.log('could not loadd:'+filename)
+// 	};
+// 	img.onload = function(){ // Loaded successfully
+// 		console.log('displaying:'+filename)
+// 	  	$('<div class="item"><img src="'+filename+'"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
+//     		$('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
 
-	};
+// 	};
     
   	}
 	//Placing the device model video
